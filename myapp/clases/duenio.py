dd79efa400509ff83488ddce5a57516450c09831
dianarmz <dianis0297@gmail.com>
2017-12-02 16:52:20 -0600

    clase mascota y duenio

--- myapp/clases/duenio.py
+++ myapp/clases/duenio.py
@@ -3,7 +3,43 @@ class Duenio(object):
     def __init__(self,nombre, userName,listMascotas,contrasenia):
         self.nombre=nombre
         self.userName=userName
-        self.listMascotas=Mascotas
+        self.listMascotas=listMascotas
         self.contrasenia=contrasenia
-        
-bla
+
+		
+	def getUsername(self):
+		return userName
+		
+		
+	def getPassword(self):
+		return contrasenia
+
+	def getMacotas(self):
+		return listMacotas
+	
+	
+		
+
+class Mascota(object):
+    def __init__(self,nombre, raza,edad,sexo,disponible):
+        self.nombre=nombre
+        self.raza=raza
+        self.edad=edad
+        self.sexo=sexo
+
+		
+	def getRaza(self):
+		return raza
+		
+		
+	def getEdad(self):
+		return edad
+
+	def getNombre(self):
+		return nombre
+		
+	
+	
+
+		
+		
