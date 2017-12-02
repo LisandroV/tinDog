9f1f0ee769146d6957ce4def468189eafe3b6de5
dianarmz <dianis0297@gmail.com>
2017-12-02 16:37:54 -0600

    clase duenio

---
+++ myapp/clases/duenio.py
@@ -0,0 +1,9 @@
+
+class Duenio(object):
+    def __init__(self,nombre, userName,listMascotas,contrasenia):
+        self.nombre=nombre
+        self.userName=userName
+        self.listMascotas=Mascotas
+        self.contrasenia=contrasenia
+        
+bla
